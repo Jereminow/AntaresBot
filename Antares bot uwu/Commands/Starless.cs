c233132c279fe4921ef9b19e5144030752bb062e
Jeremi <Jerenowi@gmail.com>
2021-03-07 15:10:19 +0100

    [add] Initialisation of the project

---
+++ Antares bot uwu/Commands/Starless.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antares_bot_uwu
+{
+    public class Starless : ModuleBase<SocketCommandContext>
+    {
+        [Command("starless")]
+        public async Task StarlessCommand() {
+            await ReplyAsync("Starless is the cutest stag ever <3");
+        }
+    }
+}
