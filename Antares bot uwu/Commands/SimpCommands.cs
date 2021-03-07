fe93780af314c5e42a78afc845c7f095712f5a44
Jeremi <Jerenowi@gmail.com>
2021-03-07 19:42:30 +0100

    [ADD] Skye and ping command

--- Antares bot uwu/Commands/SimpCommands.cs
+++ Antares bot uwu/Commands/SimpCommands.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,12 +12,22 @@ namespace Antares_bot_uwu
     {
         [Command("starless")]
         public async Task StarlessCommand() {
-            await ReplyAsync("Starless is the cutest stag ever <3");
+            await ReplyAsync("Starless is the cutest and sweetest stag ever <3");
         }
         [Command("cocaine")]
         public async Task CocaineCommand()
         {
             await ReplyAsync("Praise the Omnissiah!!!");
+        }
+        [Command("skye")]
+        public async Task SkyeCommand() {
+            Emote skyeHug = Emote.Parse("<:skyeStarlessHug:813449589897297920>");
+            await ReplyAsync(@$"Skye is the cutest woofie >w< {skyeHug}");
+        }
+        [Command("ping")]
+        public async Task PingCommand()
+        {
+            await ReplyAsync($"Pong {Context.Message.Author.Username} <@{Context.Message.Author.Id}>");
         }
     }
 }
